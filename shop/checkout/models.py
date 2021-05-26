3f8c42110f3c95a15f99b5aba8ae1a34f50328fa
andzn986 <andrzejgawlowski.q@gmail.com>
2021-05-26 17:45:54 +0200

    day4-1

--- shop/checkout/models.py
+++ shop/checkout/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Checkout(models.Model):
+    user = models.ForeignKey()
+    user_email = models.EmailField(null=False, blank=False)
+
+
+class CheckoutLine(models.Model):
+    variant = models.ForeignKey()
+    quantity = models.IntegerField(null=False, blank=False)
+    checkout = models.ForeignKey()
