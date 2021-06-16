65c080b0b42eb7fa76473bf34e116cafc817b81a
andzn986 <andrzejgawlowski.q@gmail.com>
2021-06-16 18:17:28 +0200

    8

--- shop/shop/tests/graphql/product/test_product.py
+++ shop/shop/tests/graphql/product/test_product.py
@@ -33,3 +33,38 @@ def test_product_by_id(db, client_query):
     assert product_response['description'] == product.description
     assert product_response['quantity'] == product.quantity
     assert product_response['price'] == str(product.price)
+
+
+def test_product_list(db, client_query):
+    product = Product.objects.create(
+        name="test1",
+        description="test1",
+        price=Decimal("10.00"),
+        quantity=10.00
+    )
+    product = Product.objects.create(
+        name="test2",
+        description="test2",
+        price=Decimal("20.00"),
+        quantity=15.00
+    )
+
+    response = client_query(
+        """
+        query{
+            products{
+                name
+                description
+                price
+                quantity
+            }
+        }
+        """
+    )
+    content = json.loads(response.content)
+
+    products_response = content['data']
+
+    assert products_response['description'] == product.description
+    assert products_response['price'] == str(product.price)
+    assert products_response['quantity'] == product.quantity
